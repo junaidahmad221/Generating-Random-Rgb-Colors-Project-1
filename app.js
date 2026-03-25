b115b482d3bda302303c23b245833932b43fd97d
Junaid Ahmad <junaidsial220@gmail.com>
2026-03-25 15:38:41 +0500

    added files

---
+++ app.js
@@ -0,0 +1,21 @@
+let btn = document.querySelector("button");
+btn.addEventListener("click", function(){
+
+    let h1 = document.querySelector("h1");
+    let RandomColor = GenerateColor();
+    h1.innerText = RandomColor;
+
+    let div = document.querySelector("div");
+    div.style.backgroundColor = RandomColor;
+
+    console.log("Color Changed");
+})
+
+function GenerateColor(){
+    let red = Math.floor(Math.random()*255);
+    let green = Math.floor(Math.random()*255);
+    let blue = Math.floor(Math.random()*255);
+
+    let color = `Rgb(${red}, ${green}, ${blue})`;
+    return color;
+}
